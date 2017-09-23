ea6cc8177a098341ef3a3100be3a83759a6d9288
plachetzki <david.plachetzki@unh.edu>
2017-09-23 10:14:54 -0400

    Delete scanNv.sh

--- scanNv.sh
+++
@@ -1,8 +0,0 @@
-#split
-cat Nematostella.fa | fasta_formatter -w 0 | split -l 1000  -d - Nvsplit
-#run
-parallel --jobs 10 '../interproscan.sh -i {} -d . -goterms -f TSV -T {}_temp' ::: ./Nvsplit*
-#../interproscan.sh -i ./Nvsplit01  -goterms -f TSV
-cat *tsv > allNvGOscan.tsv
-#rm Nvsplit*
-rm -rf *_temp
